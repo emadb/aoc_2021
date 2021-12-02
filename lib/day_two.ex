a2355abb2f9add272fe14224209bcd4efd063d5d
Emanuele DelBono <emanuele@codiceplastico.com>
2021-12-02 09:45:07 +0100

    refactoring day2 using recursion

--- lib/day_two.ex
+++ lib/day_two.ex
@@ -3,21 +3,23 @@ defmodule Aoc.DayTwo do
 
   def part_one(file_path) do
     input = InputReader.get_lines_string(file_path)
-    {x, d} = Enum.reduce(input, {0, 0}, &next_value_p1/2)
+    {x, d } = move(input, {0, 0})
     x * d
   end
 
-  def next_value_p1("forward " <> v, {f, d}), do: {f + to_integer(v), d}
-  def next_value_p1("down " <> v, {f, d}), do: {f, d + to_integer(v)}
-  def next_value_p1("up " <> v, {f, d}), do: {f, d - to_integer(v)}
+  def move(["forward " <> v | rest], {f, d}), do: move(rest, {f + to_integer(v), d})
+  def move(["down " <> v | rest], {f, d}), do: move(rest, {f, d + to_integer(v)})
+  def move(["up " <> v | rest], {f, d}), do: move(rest, {f, d - to_integer(v)})
+  def move([], result), do: result
 
   def part_two(file_path) do
     input = InputReader.get_lines_string(file_path)
-    {x, d, _} = Enum.reduce(input, {0, 0, 0}, &next_value_p2/2)
+    {x, d, _} = move_2(input, {0, 0, 0})
     x * d
   end
 
-  def next_value_p2("forward " <> v, {f, d, a}), do: {f + to_integer(v), d + (a * to_integer(v)), a}
-  def next_value_p2("down " <> v, {f, d, a}), do: {f, d, a + to_integer(v)}
-  def next_value_p2("up " <> v, {f, d, a}), do: {f, d, a - to_integer(v)}
+  def move_2(["forward " <> v | rest], {f, d, a}), do: move_2(rest, {f + to_integer(v), d + (a * to_integer(v)), a})
+  def move_2(["down " <> v | rest], {f, d, a}), do: move_2(rest, {f, d, a + to_integer(v)})
+  def move_2(["up " <> v | rest], {f, d, a}), do: move_2(rest, {f, d, a - to_integer(v)})
+  def move_2([], result), do: result
 end
